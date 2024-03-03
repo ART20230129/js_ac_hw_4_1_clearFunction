09d028893e7ab9b747950a74328556ce878cbf16
ART <tow1979@gmail.com>
2024-03-03 20:42:26 +0300

    initial commit

---
+++ src/__tests__/index.js
@@ -0,0 +1,33 @@
+import { heroHealth } from '../index.js';
+
+test('status - healthy', () => {
+  const list = {name: 'Маг', health: 90}
+
+  const result = heroHealth(list);
+  expect(result).toBe('healthy');
+});
+
+test('status - wounded', () => {
+        const list = {name: 'Лучник', health: 30}
+      
+        const result = heroHealth(list);
+        expect(result).toBe('wounded');
+});
+
+test('status - critical', () => {
+        const list = {name: 'Всадник', health: 10}
+      
+        const result = heroHealth(list);
+        expect(result).toBe('critical');
+});
+
+// Воторой вариант тестирования
+test.each([
+        ['Маг', 90, 'healthy'],
+        ['Лучник', 30, 'wounded'],
+        ['Всадник', 10, 'critical'],
+])
+('testing status health %s with %i - %s', (nic, quantity, status) => {
+        const result = heroHealth({name: nic, health:quantity});
+        expect(result).toBe(status);
+})
---
+++ src/index.js
@@ -0,0 +1,10 @@
+export function heroHealth(obj) {
+  if (obj.health > 50) {
+        return 'healthy';
+  };
+  if (obj.health >= 15 && obj.health <= 50){
+        return 'wounded';
+  };
+  return 'critical';
+}
+
